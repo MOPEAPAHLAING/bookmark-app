3c4399fceb31dedce456cb70eea084a0c0252551
MOEPAPAHLAING <moepapahlaing21@gmail.com>
2022-03-30 13:03:43 -0700

    Finishing The Project

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function buildBookmarks() {
         const closeIcon = document.createElement('i');
         closeIcon.classList.add('fas', 'fa-times');
         closeIcon.setAttribute('title', 'Delete Bookmark');
-        closeIcon.setAttribute('onclick', `deleteBookmark('${url})`);
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
         // Favicon / Link Container
         const linkInfo = document.createElement('div');
         linkInfo.classList.add('name');
